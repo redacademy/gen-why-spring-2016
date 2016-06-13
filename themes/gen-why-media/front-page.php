6b6a83e7314beecbd3e1200f2e04f747041085f2
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-13 16:56:24 -0700

    added media queries mixins and added icons to front page

--- themes/gen-why-media/front-page.php
+++ themes/gen-why-media/front-page.php
@@ -10,6 +10,28 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<!-- Hero Slider  -->
+
+
+			<!-- Get Involved -->
+			<section class="get-involved">
+				<h1>Why Get Involved?</h1>
+				<ul>
+					<li> <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/Community.png" alt="" /> </li>
+					<li> <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/ShapeYourCity.png" alt="" /> </li>
+					<li> <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/Collaborate.png" alt="" /> </li>
+					<li> <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/Connect.png" alt="" /> </li>
+				</ul>
+			</section>
+
+
+			<!-- What we are doing -->
+
+
+			<!-- What People Say -->
+
+
+			<!-- Latest Event Clips -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
