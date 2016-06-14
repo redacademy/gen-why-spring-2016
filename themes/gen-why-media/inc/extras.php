b9bcdfbf59d4074137bbc3d3d59b206808bcfbe9
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-14 13:29:08 -0700

    beginning work on adding custom field photos for watch

--- themes/gen-why-media/inc/extras.php
+++ themes/gen-why-media/inc/extras.php
@@ -20,3 +20,32 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+//Custom Function for setting Featured Image as Splash for About Page
+function 'header_image'() {
+
+	if( !is_page_template( 'watch.php' ) ) {
+		return ;
+	}
+
+   $custom_css = 
+           ".watchhero{
+                   background:
+                       linear-gradient( 
+                            rgba(0,0,0,0.4),
+                            rgba(0,0,0,0.4)
+                        ),
+                       url('". CFS()->get('header_image') ."');
+                   display: flex;
+                   background-size: cover;
+                   background-position: bottom;
+                   height: 25vh;
+                   width: 100vw;
+                   padding-top: 0;
+                   justify-content: center; 
+                   align-items: center;
+                   color: white;
+           }";
+   wp_add_inline_style( 'inhabitent-style', $custom_css );
+}
+add_action( 'wp_enqueue_scripts', 'header_image' );
