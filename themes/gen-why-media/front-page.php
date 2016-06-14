49a3d9f272ed5f7b5685914104c1654b9d835169
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-14 16:19:34 -0700

    style the Stay Connected With Us section, making it responsive

--- themes/gen-why-media/front-page.php
+++ themes/gen-why-media/front-page.php
@@ -136,20 +136,26 @@ get_header(); ?>
 			<section class="connect-us">
 				<h2 class="header2-a">Stay Connected With Us</h2>
 				<div class="connect-container">
+
 					<div class="twit-connect connect-wrapper">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter-icon.png" alt="Twitter" />
+						<p>TWITTERTWITTERTWITTER</p>
 						<a href="#" class="connect-btn twit-btn">Follow</a>
 					</div>
+
 					<div class="insta-connect connect-wrapper">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-icon.png" alt="Instagram" />
+						<img src="" alt="" />
 						<a href="#" class="connect-btn insta-btn">Follow</a>
 					</div>
+
 					<div class="email-connect connect-wrapper">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/mail-icon.png" alt="Email" />
 						<p>Want to know about our next event?</p>
 						<input type="text" name="name" placeholder="Type your email here...">
 						<a href="#" class="connect-btn email-btn">Subscribe</a>
 					</div>
+
 				</div><!-- .connect-container -->
 			</section><!-- .connect-us -->
 
