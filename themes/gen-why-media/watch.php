f1e7a978d47695ba7870c241ba335370dca3ebe1
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-14 14:39:02 -0700

    adds linear gradient and header to watch page

--- themes/gen-why-media/watch.php
+++ themes/gen-why-media/watch.php
@@ -10,4 +10,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<div class="watchhero">
+			<h1 class="header1">Watch<h1>
+		</div>
+
 <?php get_footer(); ?>
