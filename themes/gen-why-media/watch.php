1f0d926cd6d431193fe6195c524d803ab32de9b3
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-14 11:14:51 -0700

    adds 'watch.php' file

---
+++ themes/gen-why-media/watch.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Watch Template
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+<?php get_footer(); ?>
