c555273c799bf2011060468034fc8e90e008a35f
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-16 16:09:39 -0700

    adds buttons to watch page

--- themes/gen-why-media/watch.php
+++ themes/gen-why-media/watch.php
@@ -9,18 +9,22 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<div class="watchhero">
-			<h1 class="header1">Watch</h1>
-		</div>
-		<div class="eventbg">
-			<h2 class="header2-b">Event Clips</h2>
-		</div>
-		<div class="shortbg">
-			<h2 class="header2-b">Short Videos</h2>
-		</div>
-		<div class="docbg">
-			<h2 class="header2-b">Documentary</h2>
-		</div>
+			<div class="watchhero">
+				<h1 class="header1">Watch</h1>
+			</div>
+			<div class="eventbg watch-center">
+				<h2 class="header2-b h2-change">Event Clips</h2>
+				<a href="#" class="button button-a fom-button">Find Out More</a>		
+			</div>
+			<div class="shortbg watch-center">
+				<h2 class="header2-b h2-change">Short Videos</h2>
+				<a href="#" class="button button-a fom-button">Find Out More</a>
+			</div>
+			<div class="docbg watch-center">
+				<h2 class="header2-b h2-change">Documentary</h2>
+				<a href="#" class="button button-a fom-button">Find Out More</a>
+			</div>
+		</main>
+	</div>
 
 <?php get_footer(); ?>
