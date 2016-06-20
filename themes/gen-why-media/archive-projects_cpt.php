a36e2a01940b5d5cee0c455d74e4ca1cd48ec14a
MichaelMini <mcho911@gmail.com>
2016-06-20 14:48:18 -0700

    adding template-parts/content-past-works.php

--- themes/gen-why-media/archive-projects_cpt.php
+++ themes/gen-why-media/archive-projects_cpt.php
@@ -33,7 +33,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-					get_template_part( 'template-parts/content' );
+					get_template_part( 'template-parts/content', 'past-works' );
 				?>
 
 			<?php endwhile; ?>
