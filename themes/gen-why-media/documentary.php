d411c58bf540922b7eedaba157d2db3ef9635f65
hunterwinter <hunter_w@live.ca>
2016-06-20 11:37:46 -0700

    adds linking for watch page buttons

---
+++ themes/gen-why-media/documentary.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Watch Template
+ *
+ * @package Genwhy_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="watchhero">
+				<h1 class="header1">Event Clips</h1>
+			</div>
+			<div class="eventbg watch-center">
+				<h2 class="header2-b h2-change">Bring Your Boomers 2016</h2>
+				<a href="" class="button button-a fom-button">Find Out More</a>		
+			</div>
+			<div class=" watch-center">
+				<h2 class="header2-b h2-change">Re-Imagine CBC</h2>
+				<a href="short-videos" class="button button-a fom-button">Find Out More</a>
+			</div>
+		</main>
+	</div>
+
+<?php get_footer(); ?>
