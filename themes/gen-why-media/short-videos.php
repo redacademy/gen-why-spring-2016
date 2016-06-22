abb0e46f89643f9e27b6fce99f4b53c2ae8cef2a
hunterwinter <hunter_w@live.ca>
2016-06-22 13:52:11 -0700

    adds header backgroud image for short-videos page

--- themes/gen-why-media/short-videos.php
+++ themes/gen-why-media/short-videos.php
@@ -12,13 +12,13 @@ get_header(); ?>
 			<div class="watchhero">
 				<h1 class="header1">Short Videos</h1>
 			</div>
-			<div class="eventbg watch-center">
-				<h2 class="header2-b h2-change">Bring Your Boomers 2016</h2>
-				<a href="" class="button button-a fom-button">Find Out More</a>		
+			<div id="scroll-target" class="docbg watch-center">
+				<h2 class="header2-b h2-change"><?php the_title(); ?></h2>
+				<a href="event-clips" class="button button-a fom-button">Find Out More</a>
 			</div>
-			<div class=" watch-center">
-				<h2 class="header2-b h2-change">Re-Imagine CBC</h2>
-				<a href="" class="button button-a fom-button">Find Out More</a>
+			<div class="shortbg watch-center">
+				<h2 class="header2-b h2-change"></h2>
+				<a href="short-videos" class="button button-a fom-button">Find Out More</a>
 			</div>
 		</main>
 	</div>
