6685cfc305fa832aaee1fe0f6f089a6d05f1f3a3
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-22 20:41:45 -0700

    adding styling to single video pages with the event-clips-short-videos.scss class already made

--- themes/gen-why-media/single-short_video.php
+++ themes/gen-why-media/single-short_video.php
@@ -7,7 +7,6 @@
 
 get_header(); ?>
 
-<div class="nav-background"></div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -16,13 +15,6 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'single-video' ); ?>
 
 			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
 
 		<?php endwhile; // End of the loop. ?>
 
