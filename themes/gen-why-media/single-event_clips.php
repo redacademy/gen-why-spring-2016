54691ce3cf97dded8f5ec654af40f061357b91cf
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-22 20:20:00 -0700

    creating single page video content to style and edit

--- themes/gen-why-media/single-event_clips.php
+++ themes/gen-why-media/single-event_clips.php
@@ -13,16 +13,10 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php get_template_part( 'template-parts/content', 'single-video' ); ?>
 
 			<?php the_post_navigation(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
 
 		<?php endwhile; // End of the loop. ?>
 
---
+++ themes/gen-why-media/template-parts/content-single-video.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package Genwhy_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'full' ); ?>
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php genwhy_posted_on(); ?>
+		</div><!-- .entry-meta -->
+
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php genwhy_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
