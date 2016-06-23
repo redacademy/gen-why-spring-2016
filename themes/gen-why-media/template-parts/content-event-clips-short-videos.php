0bf4eb6704972fe76bac7edfbd06b6be8d31f4f1
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-22 19:25:45 -0700

    adding styling and content to event clips and short videos php

---
+++ themes/gen-why-media/template-parts/content-event-clips-short-videos.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package Genwhy_Theme
+ */
+
+?>
+
+	<section class="video-container">
+		<div class="clip-contain">
+
+			<header class="entry-header ">
+				<div class="video-banner-imgs">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'full' ); ?>
+					<?php endif; ?>
+				</div>
+
+
+				<div class="video-title-button">
+					<?php the_title( sprintf( '<h2 class="entry-title header2-a"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					<a href="<?php echo esc_url( get_permalink() ) ?>" class="button fom-button">Find Out More</a>
+				</div>
+
+		</div><!-- .clip-contain -->
+</section><!-- .event-clip-container -->
