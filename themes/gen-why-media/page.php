6fd416714c8dee465bafaed10c5d1f6bb0709a36
MichaelMini <mcho911@gmail.com>
2016-06-22 21:22:17 -0700

    adding Current Page Highlighting to contact-us page

--- themes/gen-why-media/page.php
+++ themes/gen-why-media/page.php
@@ -3,20 +3,22 @@
  * The template for displaying all pages.
  *
  * @package Genwhy_Theme
- */
+ */?>
+<div class="contact-us-page">
 
-get_header(); ?>
+	<?get_header(); ?>
 
-<div id="primary" class="content-area contact-us-wrapper">
-	<main id="main" class="site-main contact-us-container" role="main">
+	<div id="primary" class="content-area contact-us-wrapper">
+		<main id="main" class="site-main contact-us-container" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-		<?php endwhile; // End of the loop. ?>
+			<?php endwhile; // End of the loop. ?>
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
+</div>
 <?php get_footer(); ?>
