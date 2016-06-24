0d8129059b39553e0daeac4accd00f1c71a777c1
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-24 14:23:23 -0700

    adding build pathing to the bxslider functions enqueue

--- themes/gen-why-media/functions.php
+++ themes/gen-why-media/functions.php
@@ -87,9 +87,9 @@ function genwhy_scripts() {
 
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 
-	wp_enqueue_style( 'style-bxslider', get_template_directory_uri() . '/js/js-bxslider/jquery.bxslider.css' );
+	wp_enqueue_style( 'style-bxslider', get_template_directory_uri() . '/build/js/js-bxslider/jquery.bxslider.css' );
 
-	wp_enqueue_script( 'script-bxslider', get_template_directory_uri() . '/js/js-bxslider/jquery.bxslider.min.js', array('jquery'), false, true );
+	wp_enqueue_script( 'script-bxslider', get_template_directory_uri() . '/build/js/js-bxslider/jquery.bxslider.min.js', array('jquery'), false, true );
 
 	wp_enqueue_script( 'scripts-js', get_template_directory_uri() . '/build/js/scripts.min.js', array('jquery'), false, true );
 
