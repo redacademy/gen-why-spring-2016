afa63c0db7754a38479874a4728b162f2fa981b4
hunterwinter <hunter_w@live.ca>
2016-06-24 09:27:58 -0700

    proper layout of footer code. removes unused 'documentary.php' file

--- themes/gen-why-media/footer.php
+++ themes/gen-why-media/footer.php
@@ -7,16 +7,16 @@
 
 ?>
 
-			</div><!-- #content -->
+	</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a class="wp-logo" href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a class="wp-logo" href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 	</body>
 </html>
