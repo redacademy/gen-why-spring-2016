3c58fee9a5c9e87b22884ee8f6e693dd7e78a19c
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-06-27 14:57:29 -0700

    making changes to the front page php and banner php in template parts based on the design QA for mobile. Replaced the What we do banner image and resized it, resized the get involved area to fit mobile better, deleted buttons on social media section

--- themes/gen-why-media/front-page.php
+++ themes/gen-why-media/front-page.php
@@ -57,7 +57,7 @@
 							<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/shape-your-city.png" alt="Shape Your City" />
 							<h3 class="colored-header-dark">Shape Your City</h3>
 						 </li>
-						<li>
+						<li class="collaborate-container">
 							<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/collaborate-icon.png" alt="Collaborate" class="collaborate"/>
 							<h3 class="colored-header-dark">Collaborate</h3>
 						</li>
